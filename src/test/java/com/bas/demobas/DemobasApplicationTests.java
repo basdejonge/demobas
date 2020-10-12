15852a3999c9d9256847370144caf1e54aad3219
Bas de Jonge <bas.de.jonge@heroes-corp.com>
2020-10-12 13:10:04 +0200

    Add spring boot skelloton

---
+++ src/test/java/com/bas/demobas/DemobasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bas.demobas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemobasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
